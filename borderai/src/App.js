1b42e480c94fc84d9e0acda263918882cd4d8eca
JezrrelValles <jezrrel.valles@ia.center>
2023-03-09 22:15:03 -0700

    Commit

--- borderai/src/App.js
+++ borderai/src/App.js
@@ -1,9 +1,11 @@
 import React from "react"
 import { MapContainer, TileLayer, Polygon, Popup, LayersControl } from 'react-leaflet'
 import { juarez, colonias } from "./data"
+import proj4 from 'proj4';
 import './App.css';
 
-console.log(colonias.features)
+proj4.defs("EPSG:4326","+proj=longlat +datum=WGS84 +no_defs");
+proj4.defs("EPSG:25830","+proj=utm +zone=13 +datum=WGS84 +units=m +no_defs';");
 
 function App() {
   return (
@@ -38,6 +40,30 @@ function App() {
       }
       </LayersControl.Overlay>
       <LayersControl.Overlay checked name="Colonias">
+        {
+          colonias.features.map((coord) => {
+            const coordinates = coord.geometry.coordinates[0];
+
+            const transformedCoords = coordinates.map((coord) =>
+            proj4('EPSG:25830', 'EPSG:4326', coord));
+
+            const swappedCoords = transformedCoords.map((coord) => [coord[1], coord[0]]);
+
+            return (
+              <Polygon
+              pathOptions={{
+                fillColor: "#f23302",
+                fillOpacity: 0.7,
+                weight: 2,
+                opacity: 1,
+                dashArray: "3",
+                color: 'black'
+              }}
+              positions={swappedCoords}>
+              </Polygon>
+            )
+          })
+        }
       </LayersControl.Overlay>
     </LayersControl>
     </MapContainer>
