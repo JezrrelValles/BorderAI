74063ad89f1a18c706830ad2774fbca0b69dbc16
JezrrelValles <jezrrel.valles@ia.center>
2023-03-15 21:48:51 -0600

    Commit

---
+++ borderai/src/App.jsx
@@ -0,0 +1,102 @@
+import React from "react";
+import MapComponent from "./MapComponent";
+import { Container, Row, Col, Table, Tabs, Tab } from "react-bootstrap";
+import { juarez, colonias, distrito5, seccionesDistrito5, datos } from "./data";
+import "./App.css";
+
+function App() {
+  return (
+    <Container fluid>
+      <Row>
+        <Tabs defaultActiveKey="demografico" className="mb-3" justify>
+          <Tab eventKey="demografico" title="Demográfico">
+            <Row>
+              <Col></Col>
+              <Col>
+                <MapComponent/>
+              </Col>
+            </Row>
+          </Tab>
+          <Tab eventKey="politico" title="Político">
+            <Row>
+              <Col sm={4}>
+                <h1>Datos</h1>
+                <Table responsive="sm">
+                  <thead>
+                    <tr>
+                      {/* <th>Departamento</th> */}
+                      {/* <th>Clave Municipio</th> */}
+                      {/* <th>Municipio</th> */}
+                      <th>Sección</th>
+                      <th>Casilla</th>
+                      {/* <th>Lista Nominal</th> */}
+                      <th>PAN</th>
+                      <th>PRI</th>
+                      <th>PRD</th>
+                      <th>PVEM</th>
+                      <th>PT</th>
+                      <th>MC</th>
+                      <th>MORENA</th>
+                      <th>PES</th>
+                      <th>RSP</th>
+                      <th>FPM</th>
+                      <th>NACH</th>
+                      <th>PAN-PRD</th>
+                      <th>PT-MORENA-NACH</th>
+                      <th>CNR</th>
+                      <th>Votos Nulos</th>
+                      <th>Votos Totales</th>
+                      <th>% Participación</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    {datos.map((item) => (
+                      <tr>
+                        {/* <td>{item.departamento}</td>
+                         <td>{item.clave_municipio}</td>
+                         <td>{item.municipio}</td> */}
+                        <td>{item.seccion}</td>
+                        <td>{item.casilla}</td>
+                        {/* <td>{item.lista_nominal}</td> */}
+                        <td>{item.pan}</td>
+                        <td>{item.pri}</td>
+                        <td>{item.prd}</td>
+                        <td>{item.pvem}</td>
+                        <td>{item.pt}</td>
+                        <td>{item.mc}</td>
+                        <td>{item.morena}</td>
+                        <td>{item.pes}</td>
+                        <td>{item.rsp}</td>
+                        <td>{item.fpm}</td>
+                        <td>{item.nach}</td>
+                        <td>{item.coalicion_pan_prd}</td>
+                        <td>{item.coalicion_pt_morena_nach}</td>
+                        <td>{item.candidato_no_registrado}</td>
+                        <td>{item.votos_nulos}</td>
+                        <td>{item.votacion_total}</td>
+                        <td>{item.porcentaje_participacion}</td>
+                      </tr>
+                    ))}
+                  </tbody>
+                </Table>
+              </Col>
+              <Col>
+                <MapComponent/>
+              </Col>
+            </Row>
+          </Tab>
+          <Tab eventKey="psicologico" title="Psicológico">
+            <Row>
+              <Col></Col>
+              <Col>
+                <MapComponent/>
+              </Col>
+            </Row>
+          </Tab>
+        </Tabs>
+      </Row>
+    </Container>
+  );
+}
+
+export default App;
