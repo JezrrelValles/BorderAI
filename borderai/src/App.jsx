995e0b0779d0cc7289464c771bc9f29f6457dcab
JezrrelValles <jezrrel.valles@ia.center>
2023-03-21 11:11:57 -0600

    Commit

--- borderai/src/App.jsx
+++ borderai/src/App.jsx
@@ -3,15 +3,38 @@ import MapComponent from "./MapComponent";
 import ColumnChart from "./ColumnChart";
 import PastelChart from "./PieChart";
 import LineaChart from "./LineChart";
-import DataTable from "react-data-table-component";
+import DataTable, { createTheme } from "react-data-table-component";
 import { useState, useEffect } from "react";
-import { Container, Row, Col, Table, Tabs, Tab } from "react-bootstrap";
+import { Container, Row, Col, Tabs, Tab } from "react-bootstrap";
 import Form from "react-bootstrap/Form";
 import { datos, columns } from "./data";
 import "./App.css";
 
+createTheme('solarized', {
+  text: {
+    primary: '#268bd2',
+    secondary: '#268bd2',
+  },
+  background: {
+    default: '#f2efe9',
+  },
+  context: {
+    background: '#cb4b16',
+    text: '#FFFFFF',
+  },
+  divider: {
+    default: '#073642',
+  },
+  button: {
+		default: 'rgba(37,135,203,.54)',
+		focus: 'rgba(37,135,203,.12)',
+		hover: 'rgba(37,135,203,.12)',
+		disabled: 'rgba(37,135,203,.18)',
+	},
+}, 'dark');
+
 function App() {
-  const [activeTab, setActiveTab] = useState(2);
+  const [activeTab, setActiveTab] = useState(1);
   const [searchText, setSearchText] = useState("");
 
   const handleSearch = (event) => {
@@ -31,7 +54,7 @@ function App() {
   }, [activeTab]);
 
   return (
-    <Container fluid>
+    <Container fluid style={{ backgroundColor: '#f2efe9' }}>
       <Row>
         <Tabs
           activeKey={activeTab}
@@ -41,8 +64,42 @@ function App() {
         >
           <Tab eventKey={0} title="Demográfico">
             <Row>
-              <Col>
-                <ColumnChart />
+              <Col sm={6}>
+                <Row>
+                  <Col></Col>
+                  <Col></Col>
+                  <Col xs={4}>
+                    <Form.Control
+                      size="sm"
+                      type="text"
+                      placeholder="Buscar"
+                      value={searchText}
+                      onChange={handleSearch}
+                    />
+                  </Col>
+                </Row>
+                <DataTable
+                  title="Demo"
+                  columns={columns}
+                  data={filteredData ? filteredData : datos}
+                  pagination={true}
+                  dense={true}
+                />
+                <LineaChart />
+                <Row className="mt-4">
+                  <Col xs={2}></Col>
+                  <Col>
+                    <ColumnChart />
+                  </Col>
+                  <Col xs={2}></Col>
+                </Row>
+                <Row>
+                  <Col xs={2}></Col>
+                  <Col>
+                    <PastelChart />
+                  </Col>
+                  <Col xs={2}></Col>
+                </Row>
               </Col>
               <Col>
                 <MapComponent />
@@ -51,66 +108,43 @@ function App() {
           </Tab>
           <Tab eventKey={1} title="Político">
             <Row>
-              <Col sm={4}>
-                <h1>Datos</h1>
-                <Table responsive="sm">
-                  <thead>
-                    <tr>
-                      {/* <th>Departamento</th> */}
-                      {/* <th>Clave Municipio</th> */}
-                      {/* <th>Municipio</th> */}
-                      <th>Sección</th>
-                      <th>Casilla</th>
-                      {/* <th>Lista Nominal</th> */}
-                      <th>PAN</th>
-                      <th>PRI</th>
-                      <th>PRD</th>
-                      <th>PVEM</th>
-                      <th>PT</th>
-                      <th>MC</th>
-                      <th>MORENA</th>
-                      <th>PES</th>
-                      <th>RSP</th>
-                      <th>FPM</th>
-                      <th>NACH</th>
-                      <th>PAN-PRD</th>
-                      <th>PT-MORENA-NACH</th>
-                      <th>CNR</th>
-                      <th>Votos Nulos</th>
-                      <th>Votos Totales</th>
-                      <th>% Participación</th>
-                    </tr>
-                  </thead>
-                  <tbody>
-                    {datos.map((item) => (
-                      <tr>
-                        {/* <td>{item.departamento}</td>
-                         <td>{item.clave_municipio}</td>
-                         <td>{item.municipio}</td> */}
-                        <td>{item.seccion}</td>
-                        <td>{item.casilla}</td>
-                        {/* <td>{item.lista_nominal}</td> */}
-                        <td>{item.pan}</td>
-                        <td>{item.pri}</td>
-                        <td>{item.prd}</td>
-                        <td>{item.pvem}</td>
-                        <td>{item.pt}</td>
-                        <td>{item.mc}</td>
-                        <td>{item.morena}</td>
-                        <td>{item.pes}</td>
-                        <td>{item.rsp}</td>
-                        <td>{item.fpm}</td>
-                        <td>{item.nach}</td>
-                        <td>{item.coalicion_pan_prd}</td>
-                        <td>{item.coalicion_pt_morena_nach}</td>
-                        <td>{item.candidato_no_registrado}</td>
-                        <td>{item.votos_nulos}</td>
-                        <td>{item.votacion_total}</td>
-                        <td>{item.porcentaje_participacion}</td>
-                      </tr>
-                    ))}
-                  </tbody>
-                </Table>
+              <Col sm={6}>
+                <Row>
+                  <Col></Col>
+                  <Col></Col>
+                  <Col xs={4}>
+                    <Form.Control
+                      size="sm"
+                      type="text"
+                      placeholder="Buscar"
+                      value={searchText}
+                      onChange={handleSearch}
+                    />
+                  </Col>
+                </Row>
+                <DataTable
+                  title="Demo"
+                  columns={columns}
+                  data={filteredData ? filteredData : datos}
+                  pagination={true}
+                  dense={true}
+                  theme="solarized"
+                />
+                <LineaChart />
+                <Row className="mt-4">
+                  <Col xs={2}></Col>
+                  <Col>
+                    <ColumnChart />
+                  </Col>
+                  <Col xs={2}></Col>
+                </Row>
+                <Row>
+                  <Col xs={2}></Col>
+                  <Col>
+                    <PastelChart />
+                  </Col>
+                  <Col xs={2}></Col>
+                </Row>
               </Col>
               <Col>
                 <MapComponent />
@@ -141,8 +175,20 @@ function App() {
                   dense={true}
                 />
                 <LineaChart />
-                <ColumnChart />
-                <PastelChart />
+                <Row className="mt-4">
+                  <Col xs={2}></Col>
+                  <Col>
+                    <ColumnChart />
+                  </Col>
+                  <Col xs={2}></Col>
+                </Row>
+                <Row>
+                  <Col xs={2}></Col>
+                  <Col>
+                    <PastelChart />
+                  </Col>
+                  <Col xs={2}></Col>
+                </Row>
               </Col>
               <Col>
                 <MapComponent />
